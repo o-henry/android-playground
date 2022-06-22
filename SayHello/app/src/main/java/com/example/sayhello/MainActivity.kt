96686dc12a2a97c8971c277aff5171fbd6e4ddb1
henry <cksgod16@gmail.com>
2022-06-22 23:30:51 +0900

    init

---
+++ SayHello/app/src/main/java/com/example/sayhello/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.sayhello
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sayhello.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        binding.btnSay.setOnClickListener {  } // id 에 접근
+    }
+}
