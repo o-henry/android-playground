bf180efbd6c51b8503c151f4084e5b0602dd2a7f
henry <cksgod16@gmail.com>
2023-08-14 11:13:44 +0900

    HEAD-FIRST.7 : navigation UI

--- CatChat/app/build.gradle.kts
+++ CatChat/app/build.gradle.kts
@@ -1,17 +1,16 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("androidx.navigation.safeargs.kotlin") version "2.7.0"
 }
 
 android {
-    namespace = "com.example.secretmessage"
-    compileSdk = 34
+    namespace = "com.example.catchat"
+    compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.example.secretmessage"
+        applicationId = "com.example.catchat"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
@@ -42,8 +41,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
